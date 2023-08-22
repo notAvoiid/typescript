191da2891e73563b41094f13b5b21f69cc918422
Igor <igor.nacimento51@gmail.com>
2023-08-22 14:02:20 -0300

    Array

---
+++ src/A005-array/array.ts
@@ -0,0 +1,19 @@
+export function multiplicaArgs(...args: number[]): number {
+  return args.reduce((ac, valor) => ac * valor, 1);
+}
+
+export function concatenaStrings(...args: string[]): string {
+  return args.reduce((ac, valor) => ac + valor);
+}
+
+export function toUpperCase(...args: string[]): string[] {
+  return args.map((valor) => valor.toUpperCase());
+}
+
+const result = multiplicaArgs(1, 2, 3);
+const concatenacao = concatenaStrings('a', 'b', 'c');
+const upper = toUpperCase('a', 'b', 'c');
+
+console.log(result);
+console.log(concatenacao);
+console.log(upper);
