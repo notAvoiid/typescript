9496de67a7fa6d4ce7a8bc6551039fb3ff73033f
Igor <igor.nacimento51@gmail.com>
2023-08-24 18:22:55 -0300

    Classes em typescript

---
+++ src/A021-classes/classes.ts
@@ -0,0 +1,36 @@
+export class Empresa {
+  readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cpnj: string;
+
+  constructor(nome: string, cpnj: string) {
+    this.nome = nome;
+    this.cpnj = cpnj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Empresa('Udemy', '123');
+const colaborador1 = new Colaborador('Igor', 'Nascimento');
+const colaborador2 = new Colaborador('Maria', 'Maria');
+const colaborador3 = new Colaborador('Jose', 'Jose');
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+empresa1.adicionaColaborador(colaborador3);
+empresa1.mostrarColaboradores();
