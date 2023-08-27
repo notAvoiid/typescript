5175581b29acc0994b466ce5071a09f3b1b0ea5e
Igor <igor.nacimento51@gmail.com>
2023-08-27 19:26:35 -0300

    Interfaces

---
+++ src/A034-interfaces/interfaces.ts
@@ -0,0 +1,37 @@
+interface TipoNome {
+  nome: string;
+}
+
+interface TipoSobrenome {
+  sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+  nomeCompleto(): string;
+}
+
+// type TipoPessoa = TipoNome & TipoSobrenome & TipoNomeCompleto;
+interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto {}
+
+export class Pessoa implements TipoPessoa2 {
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+  ) {}
+
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoaObj: TipoPessoa2 = {
+  nomeCompleto() {
+    return this.nome + ' ' + this.sobrenome;
+  },
+  nome: 'Igor',
+  sobrenome: 'Nascimento',
+};
+
+const pessoa = new Pessoa('Igor', 'Nascimento');
+console.log(pessoaObj.nomeCompleto());
+console.log(pessoa.nomeCompleto());
