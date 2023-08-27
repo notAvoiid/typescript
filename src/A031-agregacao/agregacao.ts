ea17af78b8258a1f1688a69c1e93168f25db21e5
Igor <igor.nacimento51@gmail.com>
2023-08-27 18:28:57 -0300

    Composição entre classes

---
+++ src/A031-agregacao/agregacao.ts
@@ -0,0 +1,34 @@
+export class CarrinhoDeCompras {
+  private readonly produtos: Produto[] = [];
+
+  inserirProdutos(...produtos: Produto[]): void {
+    for (const produto of produtos) {
+      this.produtos.push(produto);
+    }
+  }
+
+  quantidadeProdutos(): number {
+    return this.produtos.length;
+  }
+
+  valorTotal(): number {
+    return this.produtos.reduce((soma, produto) => soma + produto._preco, 0);
+  }
+}
+
+export class Produto {
+  constructor(
+    public _nome: string,
+    public _preco: number,
+  ) {}
+}
+
+const produto1 = new Produto('Camiseta', 25.0);
+const produto2 = new Produto('Caneca', 50.0);
+const produto3 = new Produto('Blusa', 150.0);
+const carrinhoDeCompras = new CarrinhoDeCompras();
+
+carrinhoDeCompras.inserirProdutos(produto1, produto2, produto3);
+
+console.log(carrinhoDeCompras.valorTotal());
+console.log(carrinhoDeCompras.quantidadeProdutos());
